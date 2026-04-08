b1c85181ed50cc7fee0b440fa74b115717258a58
ceosricharan <74915356+ceocharan@users.noreply.github.com>
2026-04-08 09:54:10 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root.val==p.val || root.val==q.val){
+            System.out.println("vhstsn");
+            return root;
+        }
+        TreeNode t=lowestCommonAncestor(root.left,p,q);
+        TreeNode v=lowestCommonAncestor(root.right,p,q);
+        System.out.println(t+" "+v);
+        if(t==null){
+            return v;
+        }
+        else if(v==null){
+            return t;
+        }
+        else{
+            return root!=null ? root:new TreeNode();
+        }
+    }
+}
